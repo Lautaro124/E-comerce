0f81e4d9ce62025b74dc848af240a7d75e0aa2ec
lautaro.gonzalez <lautaro.gonzalez@ctl.com.ar>
2023-01-02 17:22:29 -0300

    data added

--- src/service/utils.ts
+++ src/service/utils.ts
@@ -1,1 +1,15 @@
+
 export const API_URL = 'https://fakestoreapi.com/products'
+
+export interface Product {
+  id: number,
+  title: string,
+  price: number,
+  description: string,
+  category: string,
+  image: string,
+  rating: {
+      rate: number,
+      count: number
+  }
+}
