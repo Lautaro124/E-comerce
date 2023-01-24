e36f773dec5c9d66d261f181200555b7f3012f0b
lautaro.gonzalez <lautaro.gonzalez@ctl.com.ar>
2023-01-24 17:45:32 -0300

    Bag

--- src/redux/modules/items/utils.ts
+++ src/redux/modules/items/utils.ts
@@ -2,6 +2,37 @@ import { ItemState } from "../../../interfaces/item";
 
 export const initialState: ItemState = {
   items: [],
-  cart: [],
+  cart: [
+    {
+      id:8,
+      title:"Pierced Owl Rose Gold Plated Stainless Steel Double",
+      price:10.99,
+      description:"Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel",
+      category:"jewelery",
+      image:"https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg"
+    },
+    {
+      id:8,
+      title:"Pierced Owl Rose Gold Plated Stainless Steel Double",
+      price:10.99,
+      description:"Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel",
+      category:"jewelery",
+      image:"https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg"
+    },{
+      id:8,
+      title:"Pierced Owl Rose Gold Plated Stainless Steel Double",
+      price:10.99,
+      description:"Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel",
+      category:"jewelery",
+      image:"https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg"
+    },{
+      id:8,
+      title:"Pierced Owl Rose Gold Plated Stainless Steel Double",
+      price:10.99,
+      description:"Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel",
+      category:"jewelery",
+      image:"https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg"
+    },
+  ],
   itemSelected: null,
 }
